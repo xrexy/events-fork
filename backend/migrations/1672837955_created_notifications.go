ab390e34f1ff4c970cd030eb1cc01df792d8c1da
xrexy <rexydoesimportantthings@gmail.com>
2023-01-04 19:04:05 +0200

    New migration files

---
+++ backend/migrations/1672837955_created_notifications.go
@@ -0,0 +1,82 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+	"github.com/pocketbase/pocketbase/models"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		jsonData := `{
+			"id": "2ftrzh49skq5l07",
+			"created": "2023-01-04 13:12:35.941Z",
+			"updated": "2023-01-04 13:12:35.941Z",
+			"name": "notifications",
+			"type": "base",
+			"system": false,
+			"schema": [
+				{
+					"system": false,
+					"id": "rgjsvnpj",
+					"name": "title",
+					"type": "text",
+					"required": false,
+					"unique": false,
+					"options": {
+						"min": null,
+						"max": null,
+						"pattern": ""
+					}
+				},
+				{
+					"system": false,
+					"id": "pynaicfq",
+					"name": "body",
+					"type": "text",
+					"required": false,
+					"unique": false,
+					"options": {
+						"min": null,
+						"max": null,
+						"pattern": ""
+					}
+				},
+				{
+					"system": false,
+					"id": "ppqmhkya",
+					"name": "read",
+					"type": "bool",
+					"required": false,
+					"unique": false,
+					"options": {}
+				}
+			],
+			"listRule": null,
+			"viewRule": null,
+			"createRule": null,
+			"updateRule": null,
+			"deleteRule": null,
+			"options": {}
+		}`
+
+		collection := &models.Collection{}
+		if err := json.Unmarshal([]byte(jsonData), &collection); err != nil {
+			return err
+		}
+
+		return daos.New(db).SaveCollection(collection)
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("2ftrzh49skq5l07")
+		if err != nil {
+			return err
+		}
+
+		return dao.DeleteCollection(collection)
+	})
+}
